400dc24210e7a69045d8956cbbb62aaf3087152b
Billion <thunderrock906@gmail.com>
2020-10-18 23:53:32 +0200

    Update botillion.py

--- botillion.py
+++ botillion.py
@@ -12,30 +12,11 @@ async def on_ready():
 
 @client.command(aliases=['8ball'])
 async def _8ball(ctx, *, question):
-    responses = ['It is certain',
-             'Without a doubt',
-             'You may rely on it',
-             'Yes definitely',
-             'It is decidedly so',
-             'As I see it, yes',
-             'Most likely',
-             'Yes',
-             'Outlook good',
-             'Signs point to yes',
-             'Reply hazy try again',
-             'Better not tell you now',
-             'Ask again later',
-             'Cannot predict now',
-             'Concentrate and ask again',
-             'Don’t count on it',
-             'Outlook not so good',
-             'My sources say no',
-             'Very doubtful',
-             'My reply is no']
-    if ctx.author.id == 147840568897044480:
+    line = open("responses.txt").read().splitlines()
+    if ctx.author.id == 123:
         await ctx.send(f'fuck yourself billion')
     else:
-        await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
+        await ctx.send(f'Question: {question}\nAnswer: {random.choice(line)}')
 
 async def getUserFromMention(mention):
     if mention.startswith('<@') and mention.endswith('>'):
